2c83c9db054e7259536d6fda977ffeb9c508cb8c
Ana Nguyen <anax@stanford.edu>
2025-09-01 20:11:04 -0700

    Squashed commit of the following:
    commit 3002d1c45f88709001458edae50cf02b626e7ad5
    Author: tntieu <116123739+tntieu@users.noreply.github.com>
    Date:   Mon Sep 1 15:14:02 2025 -0700
    
        Door fixes for lvl 3, lvl 2 updates

---
+++ Assets/Scripts/TrapReset.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TrapReset : MonoBehaviour
+{
+    public float damage;
+    private Inventory _inventory;
+
+    void Awake()
+    {
+        _inventory = FindFirstObjectByType<Inventory>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && _inventory.isSolid)
+        {
+            LevelManager.Instance.RestartScene();
+        }
+    }
+}
